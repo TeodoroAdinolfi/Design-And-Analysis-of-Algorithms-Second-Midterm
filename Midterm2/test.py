9c633c63c5fed7270088a21a4ab065b394ce404d
Teodoro Adinolfi <t.adinolfi2@studenti.unisa.it>
2022-12-27 14:58:22 +0100

    bug semidebellato

--- Midterm2/test.py
+++ Midterm2/test.py
@@ -28,10 +28,12 @@ else:
     print(end)
 
 #Testing DeviceSelection
-N = ('Device 1', 'Device 2', 'Device 3', 'Device 4', 'Device 5')
+N = ('Device 1', 'Device 2', 'Device 3', 'Device 4')
 X = 7
-data = {'Device 1': (100, 99, 85, 77, 63), 'Device 2': (101, 88, 82, 75, 60), 'Device 3': (98, 89, 84, 76, 61), 'Device 4': (110, 65, 65, 67, 80), 'Device 5': (95, 80, 80, 63, 60)}
-partition = [['Device 1', 'Device 3', 'Device 5'], ['Device 2'], ['Device 4']]
+#data = {'Device 1': (100, 99, 85, 77, 63), 'Device 2': (101, 88, 82, 75, 60), 'Device 3': (98, 89, 84, 76, 61), 'Device 4': (110, 65, 65, 67, 80), 'Device 5': (95, 80, 80, 63, 60)}
+#partition = [['Device 1', 'Device 3', 'Device 5'], ['Device 2'], ['Device 4']]
+data = {'Device 1': (5, 5, 5, 5, 5), 'Device 2': (1,1,1,1,3), 'Device 3': (4,4, 4, 4, 2), 'Device 4': (3,3,3,3,10)}
+partition = [['Device 1', 'Device 3'], ['Device 4','Device 2']]
 
 start = time()
 ds=DeviceSelection(N, X, data)
@@ -43,6 +45,7 @@ for i in range(C):
         subsets[i].append(dev)
         dev = ds.nextDevice(i)
 end=time()-start
+print(subsets)
 
 if sorted(subsets) != sorted(partition):
     print('FAIL')
--- Midterm2/testApprofonditi.py
+++ Midterm2/testApprofonditi.py
@@ -3,24 +3,35 @@ from DeviceSelection import DeviceSelection
 from time import time
 
 #Testing pos_tagging
-R=('Noun', 'Modal', 'Verb')
-S=('Will', 'Mary', 'Spot', 'Jane')
-T=dict()
-T['Start']={'Noun': 3/4, 'Modal': 1/4, 'Verb': 0, 'End': 0}
-T['Noun']={'Noun': 1/9, 'Modal': 3/9, 'Verb': 1/9, 'End': 4/9}
-T['Modal']={'Noun': 1/4, 'Modal': 0, 'Verb': 3/4, 'End': 0}
-T['Verb']={'Noun': 1, 'Modal': 0, 'Verb': 0, 'End': 0}
-E=dict()
-E['Will']={'Noun': 1/4, 'Modal': 3/4, 'Verb': 0}
-E['Mary']={'Noun': 1, 'Modal': 0, 'Verb': 0}
-E['Spot']={'Noun': 1/2, 'Modal': 0, 'Verb': 1/2}
-E['Jane']={'Noun': 1, 'Modal': 0, 'Verb': 0}
-out={'Will': 'Modal', 'Mary': 'Noun', 'Spot': 'Verb', 'Jane': 'Noun'}
+R = ('Noun', 'Modal', 'Verb', 'Article', 'Adjective')
+S = ('This', 'is', 'where', 'the', 'fun', 'begins', 'This2', 'is2', 'where2', 'the2', 'fun2', 'begins2')
+T = dict()
+T['Start'] = {'Noun': 3/5, 'Modal': 1/5, 'Verb': 0, 'Adjective': 0, 'Article': 1/5, 'End': 0}
+T['Noun'] = {'Noun': 2/10, 'Modal': 3/10, 'Verb': 2/10, 'Adjective': 0, 'Article': 1/10, 'End': 2/10}
+T['Modal'] = {'Noun': 2/6, 'Modal': 0, 'Verb': 3/6, 'Adjective': 0, 'Article': 1/6, 'End': 0}
+T['Verb'] = {'Noun': 2/6, 'Modal': 1/6, 'Verb': 0, 'Adjective': 0, 'Article': 2/6, 'End': 1/6}
+T['Adjective'] = {'Noun': 4/5, 'Modal': 0, 'Verb': 0, 'Adjective': 0, 'Article': 1/5, 'End': 0}
+T['Article'] = {'Noun': 9/10, 'Modal': 1/10, 'Verb': 0, 'Adjective': 0, 'Article': 0, 'End': 0}
+E = dict()
+E['This'] = {'Noun': 0, 'Modal': 1/2, 'Verb': 0, 'Adjective': 1/2, 'Article': 0}
+E['is'] = {'Noun': 0, 'Modal': 0, 'Verb': 1, 'Adjective': 0, 'Article': 0}
+E['where'] = {'Noun': 0, 'Modal': 1, 'Verb': 0, 'Adjective': 0, 'Article': 0}
+E['the'] = {'Noun': 0, 'Modal': 0, 'Verb': 0, 'Adjective': 0, 'Article': 1}
+E['fun'] = {'Noun': 1, 'Modal': 0, 'Verb': 0, 'Adjective': 0, 'Article': 0}
+E['begins'] = {'Noun': 1/3, 'Modal': 0, 'Verb': 2/3, 'Adjective': 0, 'Article': 0}
+E['This2'] = {'Noun': 0, 'Modal': 1/2, 'Verb': 0, 'Adjective': 1/2, 'Article': 0}
+E['is2'] = {'Noun': 0, 'Modal': 0, 'Verb': 1, 'Adjective': 0, 'Article': 0}
+E['where2'] = {'Noun': 0, 'Modal': 1, 'Verb': 0, 'Adjective': 0, 'Article': 0}
+E['the2'] = {'Noun': 0, 'Modal': 0, 'Verb': 0, 'Adjective': 0, 'Article': 1}
+E['fun2'] = {'Noun': 1, 'Modal': 0, 'Verb': 0, 'Adjective': 0, 'Article': 0}
+E['begins2'] = {'Noun': 1/3, 'Modal': 0, 'Verb': 2/3, 'Adjective': 0, 'Article': 0}
+out = {'This': 'Modal', 'is': 'Verb', 'where': 'Modal', 'the': 'Article', 'fun': 'Noun', 'begins': 'Verb', 'This2': 'Modal', 'is2': 'Verb', 'where2': 'Modal', 'the2': 'Article', 'fun2': 'Noun', 'begins2': 'Verb'}
 
 start = time()
 sol = pos_tagging(R, S, T, E)
 end = time()-start
-
+print(sol)
+print(out)
 if sol != out:
     print('FAIL')
 else:
@@ -28,24 +39,20 @@ else:
     print(end)
 
 #Testing DeviceSelection
-N = ('Device 1', 'Device 2', 'Device 3')
-X = 7
-data = {'Device 1': (101, 99, 85, 77, 63), 'Device 2': (100, 88, 82, 75, 60), 'Device 3': (101, 89, 84, 76, 61)}
-#partition = [['Device 1', 'Device 3', 'Device 5'], ['Device 2'], ['Device 4']]
+# N = ('Device 1', 'Device 2', 'Device 3')
+# X = 7
+# data = {'Device 1': (101, 99, 85, 77, 63), 'Device 2': (100, 88, 82, 75, 60), 'Device 3': (101, 89, 84, 76, 61)}
+# #partition = [['Device 1', 'Device 3', 'Device 5'], ['Device 2'], ['Device 4']]
 
-start = time()
-ds=DeviceSelection(N, X, data)
-C=ds.countDevices()
-subsets = [[] for i in range(C)]
-for i in range(C):
-    dev = ds.nextDevice(i)
-    while dev is not None:
-        subsets[i].append(dev)
-        dev = ds.nextDevice(i)
-end=time()-start
+# start = time()
+# ds=DeviceSelection(N, X, data)
+# C=ds.countDevices()
+# subsets = [[] for i in range(C)]
+# for i in range(C):
+#     dev = ds.nextDevice(i)
+#     while dev is not None:
+#         subsets[i].append(dev)
+#         dev = ds.nextDevice(i)
+# end=time()-start
 
-if sorted(subsets) != sorted(partition):
-    print('FAIL')
-else:
-    print('True')
-    print(end)
+
